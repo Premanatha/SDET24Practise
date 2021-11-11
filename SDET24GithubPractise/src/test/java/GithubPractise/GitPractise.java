e24b5e5912e81a2ddd577664c2037f1b6da319bb
Premnatha <Premnatha@DESKTOP-QGDI437.kuikdelivery.com>
2021-11-11 13:35:01 +0530

    user 1 and first commit

---
+++ SDET24GithubPractise/src/test/java/GithubPractise/GitPractise.java
@@ -0,0 +1,12 @@
+package GithubPractise;
+
+import org.testng.annotations.Test;
+
+public class GitPractise {
+	@Test
+	public void Prctise()
+	{
+		System.out.println("Hi i am the user and it is my first commit");
+	}
+
+}
